7e6725cef321872c707758e07700dc8eaf112cef
Dasha Yarkovska <dyarkovs@c2r5s5.42wolfsburg.de>
2024-05-12 17:42:14 +0200

    fix norm

--- src/lib/ft_strchr.c
+++ src/lib/ft_strchr.c
@@ -6,15 +6,19 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 16:02:48 by dyarkovs          #+#    #+#             */
-/*   Updated: 2023/12/08 15:09:27 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/05/12 17:38:16 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s != (unsigned char)c && *s)
 		s++;
 	if (*s == (unsigned char)c)
 		return ((char *)s);
-	return (0);
+	return (NULL);
 }
--- src/lib/ft_swap.c
+++ src/lib/ft_swap.c
@@ -6,17 +6,17 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:29:55 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/04/01 20:31:32 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/05/12 17:39:26 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft.h"
 
-void    ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-    int tmp;
+	int	tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
